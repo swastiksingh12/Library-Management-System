0d1ed7b72377f258f3b1783f200a3bc5be191dbd
Vedansh Tripath <Vedansha2710@gmail.com>
2026-01-24 23:21:39 +0530

    modified

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root.title("Library Management System")
 root.config(bg="lightblue")
 lb=Label(root, text="Welcome to Library Management System", font=("Arial", 24), bg="lightblue")
 lb.pack(pady=20)
-
+print("hello world")
 
 
 
