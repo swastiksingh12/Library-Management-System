20e7adef1adad80b78eef3f7ccdf7147dc8635ff
Swastik Singh <swastiksingh1981@gmail.com>
2026-01-24 23:08:00 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ root=Tk()
 root.geometry("600x500")
 root.title("Library Management System")
 root.config(bg="lightblue")
-
+lb=Label(root, text="Welcome to Library Management System", font=("Arial", 24), bg="lightblue")
+lb.pack(pady=20)
 
 
 
