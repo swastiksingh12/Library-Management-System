6cce047b6f2a70ad7f6a5398e9b3c7cdd990ea90
Swastik Singh <swastiksingh1981@gmail.com>
2026-01-27 17:12:12 +0530

    modified

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from customtkinter import *
+import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 root=Tk()
@@ -9,7 +11,7 @@ root.title("Library Management System")
 bg_image=Image.open('bg.jpg')
 bg_image=bg_image.resize((600, 500), Image.LANCZOS)
 bg_image=ImageTk.PhotoImage(bg_image)
-bg_label=Label(root, image=bg_image)
+bg_label=tk.Label(root, image=bg_image, text="", padx=0, pady=0)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 #functions
@@ -20,15 +22,17 @@ def view_books():
 def rented_book():
     pass
 
+tk.Label(bg_label, text="Library Management System", font=("Arial", 24), fg="black").pack(pady=20)
+
 
 #buttons
-add_book=Button(root, text="Add Book", font=("Arial", 16), bg="lightblue", fg="black", padx=10, pady=5, command=add_book)
+add_book=CTkButton(bg_label, text="Add Book", font=("Arial", 16), fg_color="#d9d9d9",text_color='black',corner_radius=0,bg_color='transparent', command=add_book)
 add_book.pack(padx=10, pady=10)
 
-view_books=Button(root, text="View Books", font=("Arial", 16), bg="lightgreen", fg="black", padx=10, pady=5, command=view_books)
+view_books=CTkButton(bg_label, text="View Books", font=("Arial", 16), fg_color="#d9d9d9",text_color='black',corner_radius=0,bg_color='transparent', command=view_books)
 view_books.pack(padx=10, pady=10)
 
-rented_book=Button(root, text="Rented Book", font=("Arial", 16), bg="lightcoral", fg="black", padx=10, pady=5, command=rented_book)
+rented_book=CTkButton(bg_label, text="Rented Book", font=("Arial", 16), fg_color="#d9d9d9",text_color='black',corner_radius=0,bg_color='transparent', command=rented_book)
 rented_book.pack(padx=10, pady=10)
 
 root.mainloop()
