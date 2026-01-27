5dd0fcc0dd4a155a4ce6ba2fb642812f76bf3230
Swastik Singh <swastiksingh1981@gmail.com>
2026-01-27 16:06:12 +0530

    Enhance main.py with background image and button functionalities

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from tkinter import *
+from PIL import Image, ImageTk
 root=Tk()
 root.geometry("600x500")
+root.maxsize(600,500)
 root.title("Library Management System")
-root.config(bg="lightblue")
-lb=Label(root, text="Welcome to Library Management System", font=("Arial", 24), bg="lightblue")
-lb.pack(pady=20)
-print("hello world")
+
+#background image
+bg_image=Image.open('bg.jpg')
+bg_image=bg_image.resize((600, 500), Image.LANCZOS)
+bg_image=ImageTk.PhotoImage(bg_image)
+bg_label=Label(root, image=bg_image)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+#functions
+def add_book():
+    pass
+def view_books():
+    pass
+def rented_book():
+    pass
 
 
+#buttons
+add_book=Button(root, text="Add Book", font=("Arial", 16), bg="lightblue", fg="black", padx=10, pady=5, command=add_book)
+add_book.pack(padx=10, pady=10)
 
+view_books=Button(root, text="View Books", font=("Arial", 16), bg="lightgreen", fg="black", padx=10, pady=5, command=view_books)
+view_books.pack(padx=10, pady=10)
 
+rented_book=Button(root, text="Rented Book", font=("Arial", 16), bg="lightcoral", fg="black", padx=10, pady=5, command=rented_book)
+rented_book.pack(padx=10, pady=10)
 
 root.mainloop()
